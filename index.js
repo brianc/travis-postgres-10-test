4bc68665131941d69fedabd656d58afec877dfcb
Brian M. Carlson <brian.m.carlson@gmail.com>
2019-03-07 12:11:06 -0600

    Test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { Client } = require('pg')
 async function run() {
   console.log('$PGHOST', process.env.PGHOST)
   console.log('$PGPORT', process.env.PGPORT)
+  console.log(process.env)
   const client = new Client()
   await client.connect()
   const { rows } = await client.query('SELECT version()')
@@ -11,4 +12,4 @@ async function run() {
   await client.end()
 }
 
-run().catch(e => console.log(error) || process.exit(-1))
+run().catch(error => console.log(error) || process.exit(-1))
